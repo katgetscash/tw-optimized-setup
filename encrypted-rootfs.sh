92030a2605dc0bb662c06a5d077ff29f990a024b
kat <65580100+katgetscash@users.noreply.github.com>
2021-07-08 18:45:23 -0700

    Create encrypted-rootfs.sh

---
+++ encrypted-rootfs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+  touch /.root.key
+  chmod 600 /.root.key
+  sudo dd if=/dev/urandom of=/.root.key bs=1024 count=1
+  sudo cryptsetup luksAddKey /dev/sda2 /.root.key  
+  echo "Edit /etc/crypttab, find the row that pertains to the root partition by UUID and add the key file in the third column. (ex: cr_sda1 UUID=... /.root.key)"
+  echo -e 'install_items+=" /.root.key "' | sudo tee --append /etc/dracut.conf.d/99-root-key.conf > /dev/null
+  echo "/boot/ root:root 700" >> /etc/permissions.local
+  chkstat --system --set
+  mkinitrd
