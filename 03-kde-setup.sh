a3ad51519ae4c526e219888ae7dffd5a6cf6e0e2
katgetscash <kat@linux.local>
2021-07-09 22:57:55 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: katgetscash <kat@linux.local>
    
     On branch dev
     Your branch is up to date with 'origin/dev'.
    
     Changes to be committed:
    	typechange: 01-encrypted-rootfs.sh
    	typechange: 02-custom-apps.sh
    	typechange: 03-kde-setup.sh

--- 03-kde-setup.sh
+++ 03-kde-setup.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-ln /usr/bin/kioclient5 /usr/bin/kfmclient
-ln /usr/bin/kwriteconfig5 /usr/bin/kwriteconfig
-ln -s /etc/mime.types /home/kat/.config/mimeapps.list
-kwriteconfig5 --file kdeglobals --group KDE --key SingleClick false
-xdg-mime default org.opensuse.yast.Packager.desktop application/x-rpm
-xdg-settings set default-web-browser chromium.desktop
+/home/kat/bin/startup-scripts/kde-setup.sh
