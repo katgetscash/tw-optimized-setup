5753d4bc874348c82bcc60724b466f2874b2c100
kat <65580100+katgetscash@users.noreply.github.com>
2021-07-19 20:19:30 -0700

    Add files via upload

---
+++ different.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ve
+# https://gist.github.com/MrDrMcCoy/74a03390620f7fdaff3d
+trap exit INT TERM QUIT HUP ERR
+
+# add tmpfs mount for better speed
+grep ' /tmp ' /etc/fstab >/dev/null || sudo tee -a /etc/fstab <<< "tmpfs /tmp tmpfs noatime,nodiratime"
+sudo mount -v /tmp
+
+##### remove sudo reauthentication timeout
+sudo sed -i.bak -e '$a\' -e 'Defaults timestamp_timeout=-1' -e '/Defaults timestamp_timeout=.*/d' /etc/sudoers
+
+##### set better ssh defaults
+mkdir -vp $HOME/.ssh
+tee $HOME/.ssh/config <<< "
+ForwardAgent no
+IdentitiesOnly yes
+IdentityFile $HOME/.ssh/home
+LogLevel=ERROR
+StrictHostKeyChecking=no
+UserKnownHostsFile=/dev/null
+" > /dev/null
+chmod -Rv u=rwX,g=,o= $HOME/.ssh
+
+sudo zypper dup -l --recommends --force-resolution
+
+
+#### Add user to groups
+for group in davfs2 docker input libvirt plugdev vboxusers ; do
+  sudo gpasswd -a $USER $group
+done
+
+echo "You should probably reboot"
+exit
