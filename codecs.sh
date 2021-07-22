54da22ca3de14c07355bd177f3f115e97168bb54
kat <65580100+katgetscash@users.noreply.github.com>
2021-07-21 17:47:12 -0700

    Update codecs.sh

--- codecs.sh
+++ codecs.sh
@@ -2,7 +2,7 @@
 ##https://cubiclenate.com/linux/system-configuration/pre-shared-ssh-key/
 sudo zypper ar -cf 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/' Packman
 sudo zypper ar -cf 'http://download.opensuse.org/repositories/mozilla/openSUSE_Tumbleweed/' Mozilla
-sudo zypper ar -cf 'http://opensuse-guide.org/repo/openSUSE_Tumbleweed/' libdvdcss
+#sudo zypper ar -cf 'http://opensuse-guide.org/repo/openSUSE_Tumbleweed/' libdvdcss
 
 sudo zypper refresh
 
