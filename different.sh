b83cf55765f8c16e8d42663e76e6554d04d6e5ef
kat <65580100+katgetscash@users.noreply.github.com>
2021-07-21 18:01:13 -0700

    Update different.sh

--- different.sh
+++ different.sh
@@ -2,11 +2,11 @@
 # https://gist.github.com/MrDrMcCoy/74a03390620f7fdaff3d
 trap exit INT TERM QUIT HUP ERR
 
-# add tmpfs mount for better speed
-grep ' /tmp ' /etc/fstab >/dev/null || sudo tee -a /etc/fstab <<< "tmpfs /tmp tmpfs noatime,nodiratime"
-sudo mount -v /tmp
+# add tmpfs mount for better speed (no longer needed)
+# grep ' /tmp ' /etc/fstab >/dev/null || sudo tee -a /etc/fstab <<< "tmpfs /tmp tmpfs noatime,nodiratime"
+# sudo mount -v /tmp
 
-##### remove sudo reauthentication timeout
+##### remove sudo reauthentication tmeout
 sudo sed -i.bak -e '$a\' -e 'Defaults timestamp_timeout=-1' -e '/Defaults timestamp_timeout=.*/d' /etc/sudoers
 
 ##### set better ssh defaults
@@ -22,7 +22,6 @@ UserKnownHostsFile=/dev/null
 chmod -Rv u=rwX,g=,o= $HOME/.ssh
 
 sudo zypper dup -l --recommends --force-resolution
-
 
 #### Add user to groups
 for group in davfs2 docker input libvirt plugdev vboxusers ; do
